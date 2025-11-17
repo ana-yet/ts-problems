dc987cd5891728676b959a501449281342560c05
ana-yet <anayethusenaj@gmail.com>
2025-11-17 12:07:22 +0600

    problem one formatvalue solved

---
+++ problems.ts
@@ -0,0 +1,5 @@
+function formatValue(value: string | number | boolean) {
+  if (typeof value == "string") return value.toUpperCase();
+  else if (typeof value == "number") return value * 10;
+  else return !value;
+}
